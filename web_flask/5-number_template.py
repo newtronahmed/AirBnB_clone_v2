7d23610bb6d4d335c26d25dfcdfbc0cdc7091189
neutronahmed <bazubairu@st.ug.edu.gh>
2023-09-22 18:09:13 +0000

    let see

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,8 +39,6 @@ def number_template(n):
 	"""display a HTML page only if n is an integer"""
 	if type(n) == int:
 		return render_template("5-number.html", n=n)
-	else:
-		return render_template("5-error.html")
 if __name__ == "__main__":
 	app.run("0.0.0.0", port=5000)
 
